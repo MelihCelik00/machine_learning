c78e963967b57c5b6e76b7138a531c22ae79d429
Melih Safa Çelik <45492251+MelihCelik00@users.noreply.github.com>
2020-05-02 16:06:22 +0300

    Fix

--- src/Decision_Tree_Regression/decision.py
+++ src/Decision_Tree_Regression/decision.py
@@ -16,11 +16,11 @@ tree_reg = DecisionTreeRegressor()
 tree_reg.fit(x, y)
 
 #tree_reg.predict(6)
-
-y_head = tree_reg.predict(x)
+x_ = np.arange(min(x),max(x),0.01).reshape(-1,1)
+y_head = tree_reg.predict(x_)
 
 plt.scatter(x,y,color="red")
-plt.plot(x,y_head,color="green")
+plt.plot(x_,y_head,color="green")
 plt.xlabel("Tribune_level")
 plt.ylabel("Cost")
 plt.show()
